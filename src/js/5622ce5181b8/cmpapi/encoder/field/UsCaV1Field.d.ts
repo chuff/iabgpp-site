06903ffb746fae40482dcfe4032abfa6617da2b6
Chad Huff <chad@totalium.com>
2025-03-10 15:57:56 -0600

    initial commit

---
+++ src/js/5622ce5181b8/cmpapi/encoder/field/UsCaV1Field.d.ts
@@ -0,0 +1,19 @@
+export declare enum UsCaV1Field {
+    VERSION = "Version",
+    SALE_OPT_OUT_NOTICE = "SaleOptOutNotice",
+    SHARING_OPT_OUT_NOTICE = "SharingOptOutNotice",
+    SENSITIVE_DATA_LIMIT_USE_NOTICE = "SensitiveDataLimitUseNotice",
+    SALE_OPT_OUT = "SaleOptOut",
+    SHARING_OPT_OUT = "SharingOptOut",
+    SENSITIVE_DATA_PROCESSING = "SensitiveDataProcessing",
+    KNOWN_CHILD_SENSITIVE_DATA_CONSENTS = "KnownChildSensitiveDataConsents",
+    PERSONAL_DATA_CONSENTS = "PersonalDataConsents",
+    MSPA_COVERED_TRANSACTION = "MspaCoveredTransaction",
+    MSPA_OPT_OUT_OPTION_MODE = "MspaOptOutOptionMode",
+    MSPA_SERVICE_PROVIDER_MODE = "MspaServiceProviderMode",
+    GPC_SEGMENT_TYPE = "GpcSegmentType",
+    GPC_SEGMENT_INCLUDED = "GpcSegmentIncluded",
+    GPC = "Gpc"
+}
+export declare const USCAV1_CORE_SEGMENT_FIELD_NAMES: UsCaV1Field[];
+export declare const USCAV1_GPC_SEGMENT_FIELD_NAMES: UsCaV1Field[];
